43260d4dc802856777de5d354944e22abec57870
kaikai6901 <huyhoangbk64@gmail.com>
2023-08-09 18:19:17 +0700

    update

--- predict_project.py
+++ predict_project.py
@@ -1,6 +1,6 @@
 from utils.helpers import *
 from train.project.training_project import *
-
+import requests
 def process():
     client = get_mongodb_client()
     training = TrainingProject(client)
@@ -9,5 +9,6 @@ def process():
     training.store_mlflow()
     training.store_features()
     client.close()
+    requests.get('http://3.1.49.34:5001/update')
 
 process()
--- settings.py
+++ settings.py
@@ -10,5 +10,5 @@ POPULATION_COLLECTION = 'population'
 CONFIG_PATH = 'configs/configs.cfg'
 MONGODB_CONFIG_NAME = 'MongoDB'
 
-MLFLOW_URI = 'http://127.0.0.1:5000/'
+MLFLOW_URI = 'http://52.221.201.209:5000/'
 MLFLOW_EXPERIMENT = 'real_estate'
